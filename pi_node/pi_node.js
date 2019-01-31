56a345cc2c8a7e45e0692a484fd279d7add0aee8
Eldad Afik <eldad.afik@gmail.com>
2019-01-31 12:04:23 -0800

    documented based on adafruit tutorial
    EA-X1-Yoga
     On branch master
     Your branch is up to date with 'origin/master'.
    
     Changes to be committed:
    	modified:   pi_node/pi_node.js

--- pi_node/pi_node.js
+++ pi_node/pi_node.js
@@ -1,24 +1,52 @@
+/* https://learn.adafruit.com/monitor-your-home-with-the-raspberry-pi-b-plus/monitoring-your-home-via-wifi */
+ 
+/*
+  The server code in Javascript including the required Node modules: 
+  (1) the node-dht-sensor module to handle the DHT sensor as before and 
+  (2) Express to handle HTTP communications like a web server.
+*/
+
 // Require
 var sensorLib = require('node-dht-sensor');
 var express = require('express');
 var app = express();
+
+/*
+  We also include the views and public directory. The views directory is where
+  we will store the interface, while the public directory is where we will put
+  both the Javascript code and the recorded pictures:
+ */
 
 // Configure app
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jade')
 app.use(express.static(__dirname + '/public'))
 
+/* 
+  We will create a route for the interface, which will allow us to access the
+  our project:
+*/ 
 // Serve interface
 app.get('/interface', function(req, res){
     res.render('interface');
 });
 
+/*
+ We will include the Raspberry Pi version of the aREST API (http://arest.io/)
+ for us to control the Raspberry Pi via HTTP:
+*/
 var piREST = require('pi-arest')(app);
 
+/* 
+ We also give an ID and a name to your Pi:
+*/
 // Set Pi properties
 piREST.set_id('1');
 piREST.set_name('my_RPi');
 
+/*
+ Finally, we call the app.listen() function to start our application:
+ */ 
 // Make measurements from sensors
 var dht_sensor = {
     initialize: function () {
